20ac5bf3039210b8f36435cc30726fcb27588457
Pedro Perez de Ayala <ppayala@gmail.com>
2024-07-02 21:35:08 +0200

    Update version

--- scrapydweb/__version__.py
+++ scrapydweb/__version__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 __title__ = 'scrapydwebx'
-__version__ = '1.5.0'
+__version__ = '1.5.1'
 __author__ = 'payala'
 __author_email__ = 'ppayala@gmail.com'
 __url__ = 'https://github.com/payala/scrapydwebx'
